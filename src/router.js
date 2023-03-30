8a5564913ac87fd6d956580fb880547104e2ed71
Ahmed Missouri <ahmedmissouri@icloud.com>
2023-03-30 11:03:24 +0100

    .

--- src/router.js
+++ src/router.js
@@ -1,17 +1,43 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { PrismaClient } = require('@prisma/client');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-
 const router = express.Router();
+const secret = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res) => {
-    
+router.post("/register", async (req, res) => {
+  const { password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+  try {
+    const user = await prisma.user.create({
+      data: { ...req.body, password: hash },
+    });
+    res.json({ user: { id: user.id, username: user.username } });
+  } catch (e) {
+    res.status(500).json({ error: e });
+  }
 });
 
-router.post('/login', async (req, res) => {
-    
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const doesUserExist = await prisma.user.findUnique({
+    where: { username: username },
+  });
+  const doesPasswordMatch = await bcrypt.compare(
+    password,
+    doesUserExist.password
+  );
+
+  if (!doesUserExist) {
+    res.status(401).json({ error: "Invalid Username." });
+  } else if (!doesPasswordMatch) {
+    res.status(401).json({ error: "Invalid Password." });
+  } else {
+    res.json(jwt.sign(doesUserExist.username, secret));
+  }
 });
 
 module.exports = router;
